b3a6b93253321c1719f62a88c8c3d1c634bd4c6f
Jeison Martinez <jeysonm021@gmail.com>
2023-07-26 11:04:52 -0500

    crazy I'm getting crazy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { writable, type Writable } from 'svelte/store';
-	import { Midi } from '@tonejs/midi';
+	// import { Midi } from '@tonejs/midi';
 
 	let midiAccess: any;
 	let midiDevices: any;
+	let selectedInputDevice: any;
+
 	let selectedOutputDevice = writable();
 	let tracks = writable([]);
 	let isPlaying: boolean = false;
@@ -21,79 +23,105 @@
 		}
 	});
 
-	async function handleFileInput(event: any) {
-		// Load MIDI file
-		console.log('File input event:', event);
-		console.log('File input files:', event.target.files);
-		const file = event.target.files[0];
-		const reader = new FileReader();
-		reader.onload = async (event: any) => {
-			const buffer = event.target.result;
-			console.log('MIDI file buffer:', buffer);
+	// async function handleFileInput(event: any) {
+	// 	// Load MIDI file
+	// 	console.log('File input event:', event);
+	// 	console.log('File input files:', event.target.files);
+	// 	const file = event.target.files[0];
+	// 	const reader = new FileReader();
+	// 	reader.onload = async (event: any) => {
+	// 		const buffer = event.target.result;
+	// 		console.log('MIDI file buffer:', buffer);
 
-			// Parse MIDI file using @tonejs/midi
-			try {
-				console.log('Parsing MIDI file using @tonejs/midi...');
-				isLoading = true;
+	// 		// Parse MIDI file using @tonejs/midi
+	// 		try {
+	// 			console.log('Parsing MIDI file using @tonejs/midi...');
+	// 			isLoading = true;
 
-				const midi = new Midi(buffer);
-				const parsedTracks = midi.tracks.map((track) =>
-					track.notes.map((note) => ({
-						time: note.time,
-						duration: note.duration,
-						name: note.name,
-						midi: note.midi,
-						velocity: note.velocity
-					}))
-				);
+	// 			const midi = new Midi(buffer);
+	// 			const parsedTracks = midi.tracks.map((track) =>
+	// 				track.notes.map((note) => ({
+	// 					time: note.time,
+	// 					duration: note.duration,
+	// 					name: note.name,
+	// 					midi: note.midi,
+	// 					velocity: note.velocity
+	// 				}))
+	// 			);
 
-				console.log('Parsed MIDI file using @tonejs/midi:', parsedTracks);
+	// 			console.log('Parsed MIDI file using @tonejs/midi:', parsedTracks);
 
-				// Set parsed MIDI file tracks to tracks store
-				tracks.set(parsedTracks);
-			} catch (error) {
-				console.error('Error parsing MIDI file using @tonejs/midi:', error);
-			} finally {
-				isLoading = false;
-			}
-		};
-		reader.readAsArrayBuffer(file);
+	// 			// Set parsed MIDI file tracks to tracks store
+	// 			tracks.set(parsedTracks);
+	// 		} catch (error) {
+	// 			console.error('Error parsing MIDI file using @tonejs/midi:', error);
+	// 		} finally {
+	// 			isLoading = false;
+	// 		}
+	// 	};
+	// 	reader.readAsArrayBuffer(file);
+	// }
+
+	// function handleDeviceOutput(outputDevice: any) {
+	// 	// Select MIDI output device
+	// 	$selectedOutputDevice = outputDevice;
+	// 	console.log('Selected output device:', $selectedOutputDevice.name);
+	// }
+
+	// async function playMidiFile() {
+	// 	console.log('playMidiFile called');
+	// 	console.log('isPlaying:', isPlaying);
+	// 	console.log('selectedOutputDevice:', $selectedOutputDevice);
+	// 	console.log('tracks:', $tracks);
+
+	// 	if (!isPlaying && $tracks.length > 0 && $selectedOutputDevice) {
+	// 		isPlaying = true;
+	// 		const track = $tracks[0];
+	// 		const startTime = performance.now();
+	// 		let lastEventTime = 0;
+	// 		for (const event of track) {
+	// 			console.log('event:', event);
+	// 			console.log('event.time:', event.time);
+	// 			console.log('event.data:', event.data); //! event data is undefined that is why it is not playing (?)
+
+	// 			const deltaTime = event.deltaTime;
+	// 			await new Promise((resolve) => setTimeout(resolve, deltaTime));
+	// 			lastEventTime += deltaTime;
+	// 			if (event.type === 'channel') {
+	// 				$selectedOutputDevice.send(event.data);
+	// 			}
+	// 		}
+	// 		isPlaying = false;
+	// 	}
+	// }
+
+	//---------------------- Handle MIDI input ----------------------//
+	function handleDeviceSelection(inputDevice: any) {
+		// Select MIDI input device
+		selectedInputDevice = inputDevice;
+		selectedInputDevice.onmidimessage = onMIDIMessage; // Listen for MIDI messages
+		console.log('Selected input device:', selectedInputDevice.name);
+		console.log('Selected input device onmidimessage:', selectedInputDevice.onmidimessage);
 	}
 
-	function handleDeviceOutput(outputDevice: any) {
-		// Select MIDI output device
-		$selectedOutputDevice = outputDevice;
-		console.log('Selected output device:', $selectedOutputDevice.name);
-	}
+	let lastNoteTime: number;
 
-	async function playMidiFile() {
-		console.log('playMidiFile called');
-		console.log('isPlaying:', isPlaying);
-		console.log('selectedOutputDevice:', $selectedOutputDevice);
-		console.log('tracks:', $tracks);
+	function onMIDIMessage(event: any) {
+		const [command, note, velocity] = event.data;
+		console.log('Incoming MIDI message:', event.data);
 
-		if (!isPlaying && $tracks.length > 0 && $selectedOutputDevice) {
-			isPlaying = true;
-			const track = $tracks[0];
-			const startTime = performance.now();
-			let lastEventTime = 0;
-			for (const event of track) {
-				console.log('event:', event);
-				console.log('event.time:', event.time);
-				console.log('event.data:', event.data); //! event data is undefined that is why it is not playing (?)
+		if (command === 0x90) {
+			// This is a note on message, handle it
+			const currentTime = performance.now();
+			const delay = currentTime - lastNoteTime;
+			console.log('Note:', note, 'Velocity:', velocity, 'Delay:', delay);
 
-				const deltaTime = event.deltaTime;
-				await new Promise((resolve) => setTimeout(resolve, deltaTime));
-				lastEventTime += deltaTime;
-				if (event.type === 'channel') {
-					$selectedOutputDevice.send(event.data);
-				}
-			}
-			isPlaying = false;
+			lastNoteTime = currentTime;
 		}
 	}
 </script>
 
+<!-- 
 <h2>Select MIDI file:</h2>
 <input type="file" accept=".mid" on:change={handleFileInput} />
 
@@ -118,6 +146,19 @@
 			<li>
 				<button class="btn" on:click={() => handleDeviceOutput(outputDevice)}>
 					{outputDevice.name}
+				</button>
+			</li>
+		{/each}
+	</ul>
+{/if} -->
+
+<h2>Select MIDI input device:</h2>
+{#if midiDevices}
+	<ul>
+		{#each midiDevices as inputDevice}
+			<li>
+				<button class="btn" on:click={() => handleDeviceSelection(inputDevice)}>
+					{inputDevice.name}
 				</button>
 			</li>
 		{/each}
