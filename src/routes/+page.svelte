158b6af661dce0b7d8067fe4915ce23c4260d1eb
Jeison Martinez <jeysonm021@gmail.com>
2023-07-27 15:37:58 -0500

    abcjs + midi-player

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { SlideToggle } from '@skeletonlabs/skeleton';
-	import { writable } from 'svelte/store';
 
 	let midiAccess: any;
 	let midiDevices: any;
 	let selectedInputDevice: any;
 	let toggleMIDI = false;
+
+	let midiOutputDevices: any;
+	let selectedOutputDevice: any;
+	let tracks: any;
+	let isPlaying = false;
 
 	onMount(async () => {
 		// Check if Web MIDI API is available
@@ -16,6 +20,9 @@
 
 			// Get all MIDI input devices
 			midiDevices = Array.from(midiAccess.inputs.values());
+
+			// Get all MIDI output devices
+			midiOutputDevices = Array.from(midiAccess.outputs.values());
 		}
 	});
 
@@ -52,6 +59,36 @@
 	} else {
 		selectedInputDevice && (selectedInputDevice.onmidimessage = null);
 	}
+
+	//------------------ Play MIDI file logic ------------------//
+	let audioContext: any;
+	async function handleOutputDeviceSelection(outputDevice: any) {
+		// Select MIDI output device
+		selectedOutputDevice = outputDevice;
+		console.log('Selected output device:', selectedOutputDevice.name);
+	}
+
+	let file: any;
+	let fileUrl: any;
+	async function handleFileSelection(event: any) {
+		file = event.target.files;
+		console.log('handleFileSelection', file);
+
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = () => {
+				const arrayBuffer = reader.result as ArrayBuffer;
+				const blob = new Blob([arrayBuffer], { type: 'audio/midi' });
+				fileUrl = URL.createObjectURL(blob);
+				console.log('playMidi', fileUrl);
+				// isPlaying = true;
+				// selectedOutputDevice.play(blob);
+			};
+			reader.readAsArrayBuffer(file[0]);
+		}
+	}
+
+	async function playMidi() {}
 </script>
 
 {#if midiDevices && !selectedInputDevice}
@@ -59,7 +96,10 @@
 	<ul>
 		{#each midiDevices as inputDevice}
 			<li>
-				<button class="btn" on:click={() => handleDeviceSelection(inputDevice)}>
+				<button
+					class="btn bg-gradient-to-br variant-gradient-primary-secondary"
+					on:click={() => handleDeviceSelection(inputDevice)}
+				>
 					{inputDevice.name}
 				</button>
 			</li>
@@ -72,3 +112,27 @@
 	<p>{selectedInputDevice.name}</p>
 	<SlideToggle name="toggle-MIDI" bind:checked={toggleMIDI}>Listen Midi Inputs</SlideToggle>
 {/if}
+
+{#if midiOutputDevices && !selectedOutputDevice}
+	<h2>Select MIDI output device:</h2>
+	<ul>
+		{#each midiOutputDevices as outputDevice}
+			<li>
+				<button
+					class="btn variant-ghost-primary"
+					on:click={() => handleOutputDeviceSelection(outputDevice)}
+				>
+					{outputDevice.name}
+				</button>
+			</li>
+		{/each}
+	</ul>
+{/if}
+
+{#if selectedOutputDevice}
+	<h2>Selected MIDI output device:</h2>
+	<p>{selectedOutputDevice.name}</p>
+	<input type="file" accept=".mid" on:change={handleFileSelection} />
+	<button class="variant-filled-primary" on:click={playMidi}>Play</button>
+{/if}
+
